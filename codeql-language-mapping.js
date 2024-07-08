01b35785de28154f40b1bc296a5fe7174e1a72b1
mariamiah <mariah.kakande@gmail.com>
2024-07-08 20:47:31 +0200

    upload sarif

---
+++ codeql-language-mapping.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "csharp": "csharp",
+    "c#": "csharp",
+    "cpp": "cpp",
+    "c": "cpp",
+    "c++": "cpp",
+    "go": "go",
+    "java": "java",
+    "javascript": "javascript",
+    "typescript": "javascript",
+    "python": "python",
+    "ruby": "ruby",
+    "swift": "swift"
+  };
